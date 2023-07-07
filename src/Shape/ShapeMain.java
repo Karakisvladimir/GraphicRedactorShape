154ccdfd6bc1b0f24a13ef93f3b1327e02f07d3d
Vladimir <karakisvladimir@gmail.com>
2023-07-07 23:51:02 +0300

    3 variant2

--- src/Shape/ShapeMain.java
+++ src/Shape/ShapeMain.java
@@ -7,24 +7,22 @@ public class ShapeMain {
         Circle shape2 = new Circle("Circle", "yellow", 41.2);
         Printer.print(shape);
         FileShapePrinter.printFile(shape2);
-//        shape2.printColor();
-//        shape2.printShapeName();
-//        System.out.println(shape2 + "\n" + " площа круга дорівнює = " + shape2.calculateSquare());
-//        // змінюю праметри через Сеттери
-//        shape.setSideA(9);
-//        shape.setSideB(8);
-//        shape.setSideC(5);
-//
-//        System.out.println("Нова площа трикутника дорівнює =  " + shape.calculateSquare());
-//Printer.print(shape2);
-//// Роблю масив с різними фігурами і находжу їх площину
-//        Square[] squre = new Square[3];
-//        squre[0] = new Circle("Circle", " green", 5);
-//        squre[1] = new Triangle("Triangle", "Blue", 5, 5, 6);
-//        squre[2] = new Rectangle("Rectangle", "Blue", 5, 6);
-//        for (Square s : squre) {
-//            System.out.println("Площина фігури дорівнює " + s.calculateSquare());
-//        }
+
+        System.out.println(shape2 + "\n" + " площа круга дорівнює = " + shape2.calculateSquare());
+        // змінюю праметри через Сеттери
+        shape.setSideA(9);
+        shape.setSideB(8);
+        shape.setSideC(5);
+        System.out.println("Нова площа трикутника дорівнює =  " + shape.calculateSquare());
+Printer.print(shape2);
+// Роблю масив с різними фігурами і находжу їх площину
+        Square[] squre = new Square[3];
+        squre[0] = new Circle("Circle", " green", 5);
+        squre[1] = new Triangle("Triangle", "Blue", 5, 5, 6);
+        squre[2] = new Rectangle("Rectangle", "Blue", 5, 6);
+        for (Square s : squre) {
+            System.out.println("Площина фігури дорівнює " + s.calculateSquare());
+        }
  }
 
 }
